1de714efb5def82478330079207c0ab94537c0db
sainath1589 <132442565+sainath1589@users.noreply.github.com>
2025-01-02 14:29:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,26 @@
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
+# Specify the provider
+provider "google" {
+  project = "future-lane-444809-c3" # Replace with your GCP project ID
+  region  = "us-central1"         # Replace with your preferred region
+  zone    = "us-central1-a"       # Replace with your preferred zone
+}
+resource "google_compute_instance" "example" {
+  name         = "example-vm"
+  machine_type = "e2-micro" # Change as per your requirement
+  zone         = "us-central1-a" # Replace with your preferred zone
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
+      image = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts" # Debian 11 image
     }
   }
+
+  # Use the default network
+  network_interface {
+    network = "default" # Reference the default network
+
+    access_config {
+      # Ephemeral public IP
+    }
+  }
+}
